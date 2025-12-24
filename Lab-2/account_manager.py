7c1ef43c234aaa950e7d331373f95cf9d9657064
aatchamps <aatchamps@gmail.com>
2025-12-24 13:20:37 +0530

    completed Lab-2

--- Lab-2/account_manager.py
+++ Lab-2/account_manager.py
@@ -3,13 +3,18 @@ class AccountManager:
     Method to store account details into the account object passed
     """
     def fill_account_data(self, acc):
-        pass
+        acc.acc_no = input("Enter account number: ")
+        acc.name = input("Enter account holder name: ")
+        try:
+            acc.balance = float(input("Enter account balance: "))
+        except ValueError:
+            acc.balance = 0.0
 
     """
     Method to display account details from the account object passed
     """
     def display_account_data(self, acc):
-        print()
+        print() 
         print("AccNo : " + acc.acc_no)
         print("Name : " + acc.name)
         print("Balance : " + str(acc.balance))
